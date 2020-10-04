ecef3d95388aec65dc1af738a0fbb04d435037ab
TwinProduction <twin@twinnation.org>
2020-10-04 19:55:19 -0400

    Add result.Connected in integration tests

--- core/service_test.go
+++ core/service_test.go
@@ -15,6 +15,9 @@ func TestIntegrationEvaluateHealth(t *testing.T) {
 	if !result.ConditionResults[0].Success {
 		t.Errorf("Condition '%s' should have been a success", condition)
 	}
+	if !result.Connected {
+		t.Error("Because the connection has been established, result.Connected should've been true")
+	}
 	if !result.Success {
 		t.Error("Because all conditions passed, this should have been a success")
 	}
@@ -30,6 +33,9 @@ func TestIntegrationEvaluateHealthWithFailure(t *testing.T) {
 	result := service.EvaluateHealth()
 	if result.ConditionResults[0].Success {
 		t.Errorf("Condition '%s' should have been a failure", condition)
+	}
+	if !result.Connected {
+		t.Error("Because the connection has been established, result.Connected should've been true")
 	}
 	if result.Success {
 		t.Error("Because one of the conditions failed, success should have been false")
