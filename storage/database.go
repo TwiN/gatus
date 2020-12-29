b67f582e410f149b27fd1e7e5087f20637dba759
Chris Heppell <chris.john.heppell@gmail.com>
2020-12-29 19:58:41 +0000

    define ResultGetter and ResultStorer interfaces and implement

---
+++ storage/database.go
@@ -0,0 +1,78 @@
+package storage
+
+import (
+	"fmt"
+
+	"github.com/TwinProduction/gatus/core"
+	"github.com/TwinProduction/gatus/storage/sql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+// ResultGetter returns gatus Results from the persistence layer
+type ResultGetter interface {
+	GetAll() (map[string]*core.ServiceStatus, error)
+}
+
+// ResultStorer stores the given result in the persistence layer
+type ResultStorer interface {
+	Store(service *core.Service, res *core.Result) error
+}
+
+// Database represents the gatus database and provides CRUD operations
+type Database struct {
+	database *gorm.DB
+}
+
+// NewDatabase constructs and returns a new Database. It will also auto-migrate the database schema if applicable
+func NewDatabase() (*Database, error) {
+	db, err := sql.NewPostgresDatabase()
+	if err != nil {
+		return nil, err
+	}
+	if err := db.AutoMigrate(&service{}, &evaluationError{}, &conditionResult{}, &result{}); err != nil {
+		return nil, err
+	}
+	return &Database{db}, nil
+}
+
+// GetAll returns all gatus results from teh database
+func (db *Database) GetAll() (map[string]*core.ServiceStatus, error) {
+	services := []service{}
+
+	queryResult := db.database.Preload("Results.ConditionResults").Preload("Results.Errors").Preload(clause.Associations).Find(&services)
+	if queryResult.Error != nil {
+		return nil, queryResult.Error
+	}
+
+	svcToResults := make(map[string]*core.ServiceStatus)
+
+	for _, svc := range services {
+		key := fmt.Sprintf("%s_%s", svc.Group, svc.Name)
+		crs := []*core.Result{}
+		for _, r := range svc.Results {
+			cr := ConvertFromStorage(r)
+			crs = append(crs, &cr)
+		}
+		svcToResults[key] = &core.ServiceStatus{
+			Name:    svc.Name,
+			Group:   svc.Group,
+			Results: crs,
+		}
+	}
+
+	return svcToResults, nil
+}
+
+// Store inserts the provided result in the database
+func (db *Database) Store(svc *core.Service, res *core.Result) error {
+	svcFromDb := &service{}
+
+	queryResult := db.database.Where(service{Name: svc.Name, Group: svc.Group}).FirstOrCreate(&svcFromDb)
+	if queryResult.Error != nil {
+		return queryResult.Error
+	}
+
+	r := ConvertToStorage(*res)
+	return db.database.Model(&svcFromDb).Association("Results").Append([]result{r})
+}
--- storage/types.go
+++ storage/types.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"time"
 
+	"github.com/TwinProduction/gatus/core"
 	"gorm.io/gorm"
 )
 
@@ -36,4 +37,61 @@ type conditionResult struct {
 	Condition string
 	Success   bool
 	ResultID  uint
+}
+
+// ConvertFromStorage converts storage structs to core structs
+func ConvertFromStorage(res result) core.Result {
+	errors := make([]string, len(res.Errors))
+	for i, err := range res.Errors {
+		errors[i] = err.Message
+	}
+
+	crs := make([]*core.ConditionResult, len(res.ConditionResults))
+	for i, cr := range res.ConditionResults {
+		crs[i] = &core.ConditionResult{
+			Condition: cr.Condition,
+			Success:   cr.Success,
+		}
+	}
+
+	return core.Result{
+		HTTPStatus:       res.HTTPStatus,
+		Body:             nil,
+		Hostname:         res.Hostname,
+		IP:               "",
+		Connected:        false,
+		Duration:         res.Duration,
+		Errors:           errors,
+		ConditionResults: crs,
+		Success:          res.Success,
+		Timestamp:        res.Timestamp,
+	}
+}
+
+// ConvertToStorage converts core structs to storage structs
+func ConvertToStorage(res core.Result) result {
+	errors := make([]evaluationError, len(res.Errors))
+	for i, err := range res.Errors {
+		errors[i] = evaluationError{
+			Message: err,
+		}
+	}
+
+	crs := make([]conditionResult, len(res.ConditionResults))
+	for i, cr := range res.ConditionResults {
+		crs[i] = conditionResult{
+			Condition: cr.Condition,
+			Success:   cr.Success,
+		}
+	}
+
+	return result{
+		HTTPStatus:       res.HTTPStatus,
+		Hostname:         res.Hostname,
+		Duration:         res.Duration,
+		Errors:           errors,
+		ConditionResults: crs,
+		Success:          res.Success,
+		Timestamp:        res.Timestamp,
+	}
 }
