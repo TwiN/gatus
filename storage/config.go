42825b62fb11cf7f02a4c1dc33332da535d422af
TwinProduction <twin@twinnation.org>
2021-02-20 19:00:54 -0500

    Update documentation

--- storage/config.go
+++ storage/config.go
@@ -2,6 +2,7 @@ package storage
 
 // Config is the configuration for alerting providers
 type Config struct {
-	// File is the path of the file to use when using file.Store
+	// File is the path of the file to use for persistence
+	// If blank, persistence is disabled.
 	File string `yaml:"file"`
 }
