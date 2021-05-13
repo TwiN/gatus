0545a4f9b52b14ebdebcebf861272af747a37980
Matej Baco <matejbacocom@gmail.com>
2021-05-13 13:00:53 +0200

    Cors

--- controller/cors.go
+++ controller/cors.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func developmentCorsHandler(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8081")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		next.ServeHTTP(w, r)
 	})
 }
