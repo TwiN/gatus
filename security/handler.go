7bc381b3569d43ad936c2a9a25665536ee6a0541
TwinProduction <twin@twinnation.org>
2021-05-24 21:46:00 -0400

    Fix typo

--- security/handler.go
+++ security/handler.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-// Handler takes care of security for a given handler with the given security configuratioon
+// Handler takes care of security for a given handler with the given security configuration
 func Handler(handler http.HandlerFunc, security *Config) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		usernameEntered, passwordEntered, ok := r.BasicAuth()
