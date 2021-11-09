a0ec6941ab3c13112d31a9720b2d9b43544f460a
TwiN <twin@twinnation.org>
2021-11-08 20:57:58 -0500

    Display number of days rather than hours if >72h

--- web/app/src/mixins/helper.js
+++ web/app/src/mixins/helper.js
@@ -2,7 +2,11 @@ export const helper = {
 	methods: {
 		generatePrettyTimeAgo(t) {
 			let differenceInMs = new Date().getTime() - new Date(t).getTime();
-			if (differenceInMs > 3600000) {
+			if (differenceInMs > 3*86400000) { // If it was more than 3 days ago, we'll display the number of days ago
+                let days = (differenceInMs / 86400000).toFixed(0);
+                return days + " day" + (days !== "1" ? "s" : "") + " ago";
+            }
+			if (differenceInMs > 3600000) { // If it was more than 1h ago, display the number of hours ago
 				let hours = (differenceInMs / 3600000).toFixed(0);
 				return hours + " hour" + (hours !== "1" ? "s" : "") + " ago";
 			}
