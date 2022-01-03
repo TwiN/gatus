3e5e66d9fca7d2ec5c24b7b0d42eb82b9b4c34f3
TwiN <twin@twinnation.org>
2022-01-02 19:22:17 -0500

    chore: Upgrade Tailwind from v2 to v3

--- web/app/tailwind.config.js
+++ web/app/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './public/index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}'
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     fontFamily: {
