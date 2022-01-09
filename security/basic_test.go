37af22f81074caee2aa199f4f65af28522a2d89b
TwiN <twin@twinnation.org>
2022-01-08 19:52:55 -0500

    test(security): Add tests for basic auth with bcrypt

--- security/basic_test.go
+++ security/basic_test.go
@@ -2,7 +2,7 @@ package security
 
 import "testing"
 
-func TestBasicConfig_IsValid(t *testing.T) {
+func TestBasicConfig_IsValidUsingSHA512(t *testing.T) {
 	basicConfig := &BasicConfig{
 		Username:           "admin",
 		PasswordSha512Hash: Sha512("test"),
@@ -12,10 +12,30 @@ func TestBasicConfig_IsValid(t *testing.T) {
 	}
 }
 
-func TestBasicConfig_IsValidWhenPasswordIsInvalid(t *testing.T) {
+func TestBasicConfig_IsValidWhenPasswordIsInvalidUsingSHA512(t *testing.T) {
 	basicConfig := &BasicConfig{
 		Username:           "admin",
 		PasswordSha512Hash: "",
+	}
+	if basicConfig.isValid() {
+		t.Error("basicConfig shouldn't have been valid")
+	}
+}
+
+func TestBasicConfig_IsValidUsingBcrypt(t *testing.T) {
+	basicConfig := &BasicConfig{
+		Username:                        "admin",
+		PasswordBcryptHashBase64Encoded: "JDJhJDA4JDFoRnpPY1hnaFl1OC9ISlFsa21VS09wOGlPU1ZOTDlHZG1qeTFvb3dIckRBUnlHUmNIRWlT",
+	}
+	if !basicConfig.isValid() {
+		t.Error("basicConfig should've been valid")
+	}
+}
+
+func TestBasicConfig_IsValidWhenPasswordIsInvalidUsingBcrypt(t *testing.T) {
+	basicConfig := &BasicConfig{
+		Username:                        "admin",
+		PasswordBcryptHashBase64Encoded: "",
 	}
 	if basicConfig.isValid() {
 		t.Error("basicConfig shouldn't have been valid")
