c682520dd94290bc1cc5bab3f53f95cdc56f9086
TwiN <twin@twinnation.org>
2022-01-13 18:42:19 -0500

    fix(security): Use LRU eviction policy for OIDC sessions

--- security/sessions.go
+++ security/sessions.go
@@ -2,4 +2,4 @@ package security
 
 import "github.com/TwiN/gocache/v2"
 
-var sessions = gocache.NewCache() // TODO: Move this to storage
+var sessions = gocache.NewCache().WithEvictionPolicy(gocache.LeastRecentlyUsed) // TODO: Move this to storage
