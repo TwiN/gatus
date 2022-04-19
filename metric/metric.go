de9a643c64209172e8ebc792e9bad0cfbdacb755
wei840222 <wei840222@gmail.com>
2022-04-19 23:45:01 +0800

    add gatus_results_success and gatus_results_duration_seconds

--- metric/metric.go
+++ metric/metric.go
@@ -11,17 +11,36 @@ import (
 var (
 	// This will be initialized once PublishMetricsForEndpoint.
 	// The reason why we're doing this is that if metrics are disabled, we don't want to initialize it unnecessarily.
-	resultCount *prometheus.CounterVec = nil
+	resultCount         *prometheus.CounterVec
+	resultSuccessGauge  *prometheus.GaugeVec
+	resultDurationGauge *prometheus.GaugeVec
 )
 
-// PublishMetricsForEndpoint publishes metrics for the given endpoint and its result.
-// These metrics will be exposed at /metrics if the metrics are enabled
-func PublishMetricsForEndpoint(endpoint *core.Endpoint, result *core.Result) {
+func ensurePrometheusMetrics() {
 	if resultCount == nil {
 		resultCount = promauto.NewCounterVec(prometheus.CounterOpts{
 			Name: "gatus_results_total",
 			Help: "Number of results per endpoint",
 		}, []string{"key", "group", "name", "success"})
+
+		resultSuccessGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "gatus_results_success",
+			Help: "Displays whether or not the watchdog result was a success",
+		}, []string{"key", "group", "name"})
+
+		resultDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Name: "gatus_results_duration_seconds",
+			Help: "Returns how long the watchdog took to complete in seconds",
+		}, []string{"key", "group", "name"})
 	}
+}
+
+// PublishMetricsForEndpoint publishes metrics for the given endpoint and its result.
+// These metrics will be exposed at /metrics if the metrics are enabled
+func PublishMetricsForEndpoint(endpoint *core.Endpoint, result *core.Result) {
+	ensurePrometheusMetrics()
+
 	resultCount.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, strconv.FormatBool(result.Success)).Inc()
+	resultSuccessGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(map[bool]float64{true: 1, false: 0}[result.Success])
+	resultDurationGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(float64(result.Duration.Seconds()))
 }
