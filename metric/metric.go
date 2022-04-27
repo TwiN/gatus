ab8dca86f49804122da90f8631522cfe62321641
wei.wan <wei.wan@linecorp.com>
2022-04-27 15:52:30 +0800

    add result http metrics

--- metric/metric.go
+++ metric/metric.go
@@ -2,6 +2,8 @@ package metric
 
 import (
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/TwiN/gatus/v3/core"
 	"github.com/prometheus/client_golang/prometheus"
@@ -15,6 +17,10 @@ var (
 	resultCount         *prometheus.CounterVec
 	resultSuccessGauge  *prometheus.GaugeVec
 	resultDurationGauge *prometheus.GaugeVec
+
+	resultHTTPStatusCode                         *prometheus.CounterVec
+	resultHTTPSSL                                *prometheus.GaugeVec
+	resultHTTPSSLLastChainExpiryTimestampSeconds *prometheus.GaugeVec
 )
 
 func ensurePrometheusMetrics() {
@@ -38,6 +44,24 @@ func ensurePrometheusMetrics() {
 			Name:      "results_duration_seconds",
 			Help:      "Returns how long the watchdog took to complete in seconds",
 		}, []string{"key", "group", "name"})
+
+		resultHTTPStatusCode = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_http_status_code",
+			Help:      "Response HTTP status code",
+		}, []string{"key", "group", "name", "url", "code"})
+
+		resultHTTPSSL = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "results_http_ssl",
+			Help:      "Indicates if SSL was used for the final redirect",
+		}, []string{"key", "group", "name", "url"})
+
+		resultHTTPSSLLastChainExpiryTimestampSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "results_http_ssl_last_chain_expiry_timestamp_seconds",
+			Help:      "Returns last SSL chain expiry in timestamp seconds",
+		}, []string{"key", "group", "name", "url"})
 	}
 }
 
@@ -49,4 +73,26 @@ func PublishMetricsForEndpoint(endpoint *core.Endpoint, result *core.Result) {
 	resultCount.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, strconv.FormatBool(result.Success)).Inc()
 	resultSuccessGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(map[bool]float64{true: 1, false: 0}[result.Success])
 	resultDurationGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(float64(result.Duration.Seconds()))
+
+	switch {
+	// DNS
+	case endpoint.DNS != nil:
+	// TCP
+	case strings.HasPrefix(endpoint.URL, "tcp://"):
+	// ICMP
+	case strings.HasPrefix(endpoint.URL, "icmp://"):
+	// STARTTLS
+	case strings.HasPrefix(endpoint.URL, "starttls://"):
+	// TLS
+	case strings.HasPrefix(endpoint.URL, "tls://"):
+	// HTTP
+	default:
+		resultHTTPStatusCode.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL, strconv.Itoa(result.HTTPStatus)).Inc()
+		if result.CertificateExpiration != 0 {
+			resultHTTPSSL.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(1)
+			resultHTTPSSLLastChainExpiryTimestampSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(float64(result.CertificateExpiration / time.Second))
+		} else {
+			resultHTTPSSL.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(0)
+		}
+	}
 }
