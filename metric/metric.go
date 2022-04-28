64b08a094ae178b34e9504fc3d1bb5714d70624c
wei.wan <wei.wan@linecorp.com>
2022-04-28 17:26:24 +0800

    add more metrics

--- metric/metric.go
+++ metric/metric.go
@@ -18,8 +18,18 @@ var (
 	resultSuccessGauge  *prometheus.GaugeVec
 	resultDurationGauge *prometheus.GaugeVec
 
+	resultDNSReturnCode *prometheus.CounterVec
+
+	resultTCPConnected *prometheus.CounterVec
+
+	resultStartTLSConnected                          *prometheus.CounterVec
+	resultStartTLSSSLLastChainExpiryTimestampSeconds *prometheus.GaugeVec
+
+	resultTLSConnected                          *prometheus.CounterVec
+	resultTLSSSLLastChainExpiryTimestampSeconds *prometheus.GaugeVec
+
+	resultHTTPConnected                          *prometheus.CounterVec
 	resultHTTPStatusCode                         *prometheus.CounterVec
-	resultHTTPSSL                                *prometheus.GaugeVec
 	resultHTTPSSLLastChainExpiryTimestampSeconds *prometheus.GaugeVec
 )
 
@@ -31,31 +41,67 @@ func ensurePrometheusMetrics() {
 			Namespace: namespace,
 			Name:      "results_total",
 			Help:      "Number of results per endpoint",
-		}, []string{"key", "group", "name", "success"})
+		}, []string{"key", "group", "name", "url", "success"})
 
 		resultSuccessGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "results_success",
 			Help:      "Displays whether or not the watchdog result was a success",
-		}, []string{"key", "group", "name"})
+		}, []string{"key", "group", "name", "url"})
 
 		resultDurationGauge = promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: namespace,
 			Name:      "results_duration_seconds",
 			Help:      "Returns how long the watchdog took to complete in seconds",
-		}, []string{"key", "group", "name"})
+		}, []string{"key", "group", "name", "url"})
+
+		resultDNSReturnCode = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_dns_return_code",
+			Help:      "Response DNS return code",
+		}, []string{"key", "group", "name", "url", "code"})
+
+		resultTCPConnected = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_tcp_connected",
+			Help:      "Response TCP connected count",
+		}, []string{"key", "group", "name", "url"})
+
+		resultStartTLSConnected = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_starttls_connected",
+			Help:      "Response StartTLS connected count",
+		}, []string{"key", "group", "name", "url"})
+
+		resultStartTLSSSLLastChainExpiryTimestampSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "results_starttls_ssl_last_chain_expiry_timestamp_seconds",
+			Help:      "Returns last SSL chain expiry in timestamp seconds",
+		}, []string{"key", "group", "name", "url"})
+
+		resultTLSConnected = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_tls_connected",
+			Help:      "Response TLS connected count",
+		}, []string{"key", "group", "name", "url"})
+
+		resultTLSSSLLastChainExpiryTimestampSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Name:      "results_tls_ssl_last_chain_expiry_timestamp_seconds",
+			Help:      "Returns last SSL chain expiry in timestamp seconds",
+		}, []string{"key", "group", "name", "url"})
+
+		resultHTTPConnected = promauto.NewCounterVec(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "results_http_connected",
+			Help:      "Response HTTP connected count",
+		}, []string{"key", "group", "name", "url"})
 
 		resultHTTPStatusCode = promauto.NewCounterVec(prometheus.CounterOpts{
 			Namespace: namespace,
 			Name:      "results_http_status_code",
 			Help:      "Response HTTP status code",
 		}, []string{"key", "group", "name", "url", "code"})
-
-		resultHTTPSSL = promauto.NewGaugeVec(prometheus.GaugeOpts{
-			Namespace: namespace,
-			Name:      "results_http_ssl",
-			Help:      "Indicates if SSL was used for the final redirect",
-		}, []string{"key", "group", "name", "url"})
 
 		resultHTTPSSLLastChainExpiryTimestampSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
 			Namespace: namespace,
@@ -70,29 +116,45 @@ func ensurePrometheusMetrics() {
 func PublishMetricsForEndpoint(endpoint *core.Endpoint, result *core.Result) {
 	ensurePrometheusMetrics()
 
-	resultCount.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, strconv.FormatBool(result.Success)).Inc()
-	resultSuccessGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(map[bool]float64{true: 1, false: 0}[result.Success])
-	resultDurationGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name).Set(float64(result.Duration.Seconds()))
+	resultCount.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL, strconv.FormatBool(result.Success)).Inc()
+	resultSuccessGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(map[bool]float64{true: 1, false: 0}[result.Success])
+	resultDurationGauge.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(float64(result.Duration.Seconds()))
 
 	switch {
 	// DNS
 	case endpoint.DNS != nil:
+		resultDNSReturnCode.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL, result.DNSRCode).Inc()
 	// TCP
 	case strings.HasPrefix(endpoint.URL, "tcp://"):
+		if result.Connected {
+			resultTCPConnected.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Inc()
+		}
 	// ICMP
 	case strings.HasPrefix(endpoint.URL, "icmp://"):
 	// STARTTLS
 	case strings.HasPrefix(endpoint.URL, "starttls://"):
+		if result.Connected {
+			resultStartTLSConnected.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Inc()
+		}
+		if result.CertificateExpiration != 0 {
+			resultStartTLSSSLLastChainExpiryTimestampSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(float64(result.CertificateExpiration / time.Second))
+		}
 	// TLS
 	case strings.HasPrefix(endpoint.URL, "tls://"):
+		if result.Connected {
+			resultTLSConnected.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Inc()
+		}
+		if result.CertificateExpiration != 0 {
+			resultTLSSSLLastChainExpiryTimestampSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(float64(result.CertificateExpiration / time.Second))
+		}
 	// HTTP
 	default:
+		if result.Connected {
+			resultHTTPConnected.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Inc()
+		}
 		resultHTTPStatusCode.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL, strconv.Itoa(result.HTTPStatus)).Inc()
 		if result.CertificateExpiration != 0 {
-			resultHTTPSSL.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(1)
 			resultHTTPSSLLastChainExpiryTimestampSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(float64(result.CertificateExpiration / time.Second))
-		} else {
-			resultHTTPSSL.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, endpoint.URL).Set(0)
 		}
 	}
 }
