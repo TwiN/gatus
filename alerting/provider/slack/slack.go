f21e459cf7a05264b5dcda67fbe0cceddaf91426
mani9223-oss <60106299+mani9223-oss@users.noreply.github.com>
2022-04-28 15:00:50 +0530

    Fix indent

--- alerting/provider/slack/slack.go
+++ alerting/provider/slack/slack.go
@@ -1,89 +1,89 @@
 package slack
 
 import (
-        "bytes"
-        "fmt"
-        "io"
-        "net/http"
+    "bytes"
+    "fmt"
+    "io"
+    "net/http"
 
-        "github.com/TwiN/gatus/v3/alerting/alert"
-        "github.com/TwiN/gatus/v3/client"
-        "github.com/TwiN/gatus/v3/core"
+    "github.com/TwiN/gatus/v3/alerting/alert"
+    "github.com/TwiN/gatus/v3/client"
+    "github.com/TwiN/gatus/v3/core"
 )
 
 // AlertProvider is the configuration necessary for sending an alert using Slack
 type AlertProvider struct {
-        WebhookURL string `yaml:"webhook-url"` // Slack webhook URL
+    WebhookURL string `yaml:"webhook-url"` // Slack webhook URL
 
-        // DefaultAlert is the default alert configuration to use for endpoints with an alert of the appropriate type
-        DefaultAlert *alert.Alert `yaml:"default-alert,omitempty"`
-        // Overrides is a list of Override that may be prioritized over the default configuration
-        Overrides []Override `yaml:"overrides,omitempty"`
+    // DefaultAlert is the default alert configuration to use for endpoints with an alert of the appropriate type
+    DefaultAlert *alert.Alert `yaml:"default-alert,omitempty"`
+    // Overrides is a list of Override that may be prioritized over the default configuration
+    Overrides []Override `yaml:"overrides,omitempty"`
 }
 
 // Override is a case under which the default integration is overridden
 type Override struct {
-        Group      string `yaml:"group"`
-        WebhookURL string `yaml:"webhook-url"`
+    Group      string `yaml:"group"`
+    WebhookURL string `yaml:"webhook-url"`
 }
 
 // IsValid returns whether the provider's configuration is valid
 func (provider *AlertProvider) IsValid() bool {
-        registeredGroups := make(map[string]bool)
-        if provider.Overrides != nil {
-                for _, override := range provider.Overrides {
-                        if isAlreadyRegistered := registeredGroups[override.Group]; isAlreadyRegistered || override.Group == "" || len(override.WebhookURL) == 0 {
-                                return false
-                        }
-                        registeredGroups[override.Group] = true
-                }
-        }
-        return len(provider.WebhookURL) > 0
+    registeredGroups := make(map[string]bool)
+    if provider.Overrides != nil {
+            for _, override := range provider.Overrides {
+                    if isAlreadyRegistered := registeredGroups[override.Group]; isAlreadyRegistered || override.Group == "" || len(override.WebhookURL) == 0 {
+                            return false
+                    }
+                    registeredGroups[override.Group] = true
+            }
+    }
+    return len(provider.WebhookURL) > 0
 }
 
 // Send an alert using the provider
 func (provider *AlertProvider) Send(endpoint *core.Endpoint, alert *alert.Alert, result *core.Result, resolved bool) error {
-        buffer := bytes.NewBuffer([]byte(provider.buildRequestBody(endpoint, alert, result, resolved)))
-        request, err := http.NewRequest(http.MethodPost, provider.getWebhookURLForGroup(endpoint.Group), buffer)
-        if err != nil {
-                return err
-        }
-        request.Header.Set("Content-Type", "application/json")
-        response, err := client.GetHTTPClient(nil).Do(request)
-        if err != nil {
-                return err
-        }
-        if response.StatusCode > 399 {
-                body, _ := io.ReadAll(response.Body)
-                return fmt.Errorf("call to provider alert returned status code %d: %s", response.StatusCode, string(body))
-        }
-        return err
+    buffer := bytes.NewBuffer([]byte(provider.buildRequestBody(endpoint, alert, result, resolved)))
+    request, err := http.NewRequest(http.MethodPost, provider.getWebhookURLForGroup(endpoint.Group), buffer)
+    if err != nil {
+            return err
+    }
+    request.Header.Set("Content-Type", "application/json")
+    response, err := client.GetHTTPClient(nil).Do(request)
+    if err != nil {
+            return err
+    }
+    if response.StatusCode > 399 {
+            body, _ := io.ReadAll(response.Body)
+            return fmt.Errorf("call to provider alert returned status code %d: %s", response.StatusCode, string(body))
+    }
+    return err
 }
 
 // buildRequestBody builds the request body for the provider
 func (provider *AlertProvider) buildRequestBody(endpoint *core.Endpoint, alert *alert.Alert, result *core.Result, resolved bool) string {
-        var message, color, results string
-        if resolved {
-                message = fmt.Sprintf("An alert for *%s* has been resolved after passing successfully %d time(s) in a row", endpoint.DisplayName(), alert.SuccessThreshold)
-                color = "#36A64F"
-        } else {
-                message = fmt.Sprintf("An alert for *%s* has been triggered due to having failed %d time(s) in a row", endpoint.DisplayName(), alert.FailureThreshold)
-                color = "#DD0000"
-        }
-        for _, conditionResult := range result.ConditionResults {
-                var prefix string
-                if conditionResult.Success {
-                        prefix = ":white_check_mark:"
-                } else {
-                        prefix = ":x:"
-                }
-                results += fmt.Sprintf("%s - `%s`\\n", prefix, conditionResult.Condition)
-        }
-        var description string
-        if alertDescription := alert.GetDescription(); len(alertDescription) > 0 {
-                description = ":\\n> " + alertDescription
-        }
-        return fmt.Sprintf(`{
+    var message, color, results string
+    if resolved {
+            message = fmt.Sprintf("An alert for *%s* has been resolved after passing successfully %d time(s) in a row", endpoint.DisplayName(), alert.SuccessThreshold)
+            color = "#36A64F"
+    } else {
+            message = fmt.Sprintf("An alert for *%s* has been triggered due to having failed %d time(s) in a row", endpoint.DisplayName(), alert.FailureThreshold)
+            color = "#DD0000"
+    }
+    for _, conditionResult := range result.ConditionResults {
+            var prefix string
+            if conditionResult.Success {
+                    prefix = ":white_check_mark:"
+            } else {
+                    prefix = ":x:"
+            }
+            results += fmt.Sprintf("%s - `%s`\\n", prefix, conditionResult.Condition)
+    }
+    var description string
+    if alertDescription := alert.GetDescription(); len(alertDescription) > 0 {
+            description = ":\\n> " + alertDescription
+    }
+    return fmt.Sprintf(`{
   "text": "",
   "attachments": [
     {
@@ -105,17 +105,17 @@ func (provider *AlertProvider) buildRequestBody(endpoint *core.Endpoint, alert *
 
 // getWebhookURLForGroup returns the appropriate Webhook URL integration to for a given group
 func (provider *AlertProvider) getWebhookURLForGroup(group string) string {
-        if provider.Overrides != nil {
-                for _, override := range provider.Overrides {
-                        if group == override.Group {
-                                return override.WebhookURL
-                        }
-                }
-        }
-        return provider.WebhookURL
+    if provider.Overrides != nil {
+            for _, override := range provider.Overrides {
+                    if group == override.Group {
+                            return override.WebhookURL
+                    }
+            }
+    }
+    return provider.WebhookURL
 }
 
 // GetDefaultAlert returns the provider's default alert configuration
 func (provider AlertProvider) GetDefaultAlert() *alert.Alert {
-        return provider.DefaultAlert
+    return provider.DefaultAlert
 }
