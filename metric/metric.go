cf9c00a2adec1de910f4f607a4c3c7873d289fc7
wei <wei840222@gmail.com>
2022-05-17 09:10:45 +0800

    feat(metrics): Add more metrics (#278)
    * add gatus_results_success and gatus_results_duration_seconds
    
    * add metrics namespace
    
    * add result http metrics
    
    * add more metrics
    
    * update
    
    * extract endpoint type method
    
    * initializedMetrics
    
    * remove too many metrics
    
    * update naming
    
    * chore(metrics): Refactor code and merge results_dns_return_code_total, results_http_status_code_total into results_code_total
    
    * docs(metrics): Update results_certificate_expiration_seconds description
    
    * add TestEndpoint_Type
    
    * remove name in table test
    
    Co-authored-by: TwiN <twin@linux.com>

--- metric/metric.go
+++ metric/metric.go
@@ -8,20 +8,66 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+const namespace = "gatus" // The prefix of the metrics
+
 var (
-	// This will be initialized once PublishMetricsForEndpoint.
-	// The reason why we're doing this is that if metrics are disabled, we don't want to initialize it unnecessarily.
-	resultCount *prometheus.CounterVec = nil
+	initializedMetrics bool // Whether the metrics have been initialized
+
+	resultTotal                        *prometheus.CounterVec
+	resultDurationSeconds              *prometheus.GaugeVec
+	resultConnectedTotal               *prometheus.CounterVec
+	resultCodeTotal                    *prometheus.CounterVec
+	resultCertificateExpirationSeconds *prometheus.GaugeVec
 )
+
+func initializePrometheusMetrics() {
+	resultTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "results_total",
+		Help:      "Number of results per endpoint",
+	}, []string{"key", "group", "name", "type", "success"})
+	resultDurationSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "results_duration_seconds",
+		Help:      "Duration of the request in seconds",
+	}, []string{"key", "group", "name", "type"})
+	resultConnectedTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "results_connected_total",
+		Help:      "Total number of results in which a connection was successfully established",
+	}, []string{"key", "group", "name", "type"})
+	resultCodeTotal = promauto.NewCounterVec(prometheus.CounterOpts{
+		Namespace: namespace,
+		Name:      "results_code_total",
+		Help:      "Total number of results by code",
+	}, []string{"key", "group", "name", "type", "code"})
+	resultCertificateExpirationSeconds = promauto.NewGaugeVec(prometheus.GaugeOpts{
+		Namespace: namespace,
+		Name:      "results_certificate_expiration_seconds",
+		Help:      "Number of seconds until the certificate expires",
+	}, []string{"key", "group", "name", "type"})
+}
 
 // PublishMetricsForEndpoint publishes metrics for the given endpoint and its result.
 // These metrics will be exposed at /metrics if the metrics are enabled
 func PublishMetricsForEndpoint(endpoint *core.Endpoint, result *core.Result) {
-	if resultCount == nil {
-		resultCount = promauto.NewCounterVec(prometheus.CounterOpts{
-			Name: "gatus_results_total",
-			Help: "Number of results per endpoint",
-		}, []string{"key", "group", "name", "success"})
+	if !initializedMetrics {
+		initializePrometheusMetrics()
+		initializedMetrics = true
 	}
-	resultCount.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, strconv.FormatBool(result.Success)).Inc()
+	endpointType := endpoint.Type()
+	resultTotal.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType), strconv.FormatBool(result.Success)).Inc()
+	resultDurationSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType)).Set(result.Duration.Seconds())
+	if result.Connected {
+		resultConnectedTotal.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType)).Inc()
+	}
+	if result.DNSRCode != "" {
+		resultCodeTotal.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType), result.DNSRCode).Inc()
+	}
+	if result.HTTPStatus != 0 {
+		resultCodeTotal.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType), strconv.Itoa(result.HTTPStatus)).Inc()
+	}
+	if result.CertificateExpiration != 0 {
+		resultCertificateExpirationSeconds.WithLabelValues(endpoint.Key(), endpoint.Group, endpoint.Name, string(endpointType)).Set(result.CertificateExpiration.Seconds())
+	}
 }
