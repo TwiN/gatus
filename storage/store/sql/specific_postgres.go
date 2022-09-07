TwiN/gatus

commit 01484832fc03ec348ebe0f8261cdd2a4ad8638c6

feat: Add [DOMAIN_EXPIRATION] placeholder for monitoring domain expiration using WHOIS (#325)
* feat: Add [DOMAIN_EXPIRATION] placeholder for monitoring domain expiration using WHOIS

* test: Fix issue caused by possibility of millisecond elapsed during previous tests

* test: Fix test with different behavior based on architecture

* docs: Revert accidental change to starttls example

* docs: Fix mistake in comment for Condition.hasIPPlaceholder()

storage/store/sql/specific_postgres.go
@@ -34,6 +34,7 @@ func (s *Store) createPostgresSchema() error {
 			status                 BIGINT,
 			dns_rcode              TEXT,
 			certificate_expiration BIGINT,
+			domain_expiration      BIGINT,
 			hostname               TEXT,
 			ip                     TEXT,
 			duration               BIGINT,
@@ -65,5 +66,7 @@ func (s *Store) createPostgresSchema() error {
 			UNIQUE(endpoint_id, hour_unix_timestamp)
 		)
 	`)
+	// Silent table modifications
+	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD IF NOT EXISTS domain_expiration BIGINT`)
 	return err
 }

storage/store/sql/specific_sqlite.go
@@ -34,6 +34,7 @@ func (s *Store) createSQLiteSchema() error {
 			status                 INTEGER,
 			dns_rcode              TEXT,
 			certificate_expiration INTEGER,
+		    domain_expiration      INTEGER,
 			hostname               TEXT,
 			ip                     TEXT,
 			duration               INTEGER,
@@ -65,5 +66,7 @@ func (s *Store) createSQLiteSchema() error {
 			UNIQUE(endpoint_id, hour_unix_timestamp)
 		)
 	`)
+	// Silent table modifications
+	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD domain_expiration INTEGER`)
 	return err
 }

storage/store/sql/sql.go
@@ -439,8 +439,8 @@ func (s *Store) insertEndpointResult(tx *sql.Tx, endpointID int64, result *core.
 	var endpointResultID int64
 	err := tx.QueryRow(
 		`
-			INSERT INTO endpoint_results (endpoint_id, success, errors, connected, status, dns_rcode, certificate_expiration, hostname, ip, duration, timestamp)
-			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+			INSERT INTO endpoint_results (endpoint_id, success, errors, connected, status, dns_rcode, certificate_expiration, domain_expiration, hostname, ip, duration, timestamp)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)
 			RETURNING endpoint_result_id
 		`,
 		endpointID,
@@ -450,6 +450,7 @@ func (s *Store) insertEndpointResult(tx *sql.Tx, endpointID int64, result *core.
 		result.HTTPStatus,
 		result.DNSRCode,
 		result.CertificateExpiration,
+		result.DomainExpiration,
 		result.Hostname,
 		result.IP,
 		result.Duration,
@@ -590,7 +591,7 @@ func (s *Store) getEndpointEventsByEndpointID(tx *sql.Tx, endpointID int64, page
 func (s *Store) getEndpointResultsByEndpointID(tx *sql.Tx, endpointID int64, page, pageSize int) (results []*core.Result, err error) {
 	rows, err := tx.Query(
 		`
-			SELECT endpoint_result_id, success, errors, connected, status, dns_rcode, certificate_expiration, hostname, ip, duration, timestamp
+			SELECT endpoint_result_id, success, errors, connected, status, dns_rcode, certificate_expiration, domain_expiration, hostname, ip, duration, timestamp
 			FROM endpoint_results
 			WHERE endpoint_id = $1
 			ORDER BY endpoint_result_id DESC -- Normally, we'd sort by timestamp, but sorting by endpoint_result_id is faster
@@ -608,7 +609,7 @@ func (s *Store) getEndpointResultsByEndpointID(tx *sql.Tx, endpointID int64, pag
 		result := &core.Result{}
 		var id int64
 		var joinedErrors string
-		_ = rows.Scan(&id, &result.Success, &joinedErrors, &result.Connected, &result.HTTPStatus, &result.DNSRCode, &result.CertificateExpiration, &result.Hostname, &result.IP, &result.Duration, &result.Timestamp)
+		_ = rows.Scan(&id, &result.Success, &joinedErrors, &result.Connected, &result.HTTPStatus, &result.DNSRCode, &result.CertificateExpiration, &result.DomainExpiration, &result.Hostname, &result.IP, &result.Duration, &result.Timestamp)
 		if len(joinedErrors) != 0 {
 			result.Errors = strings.Split(joinedErrors, arraySeparator)
 		}

core/condition.go
@@ -46,6 +46,9 @@ const (
 	// Values that could replace the placeholder: 4461677039 (~52 days)
 	CertificateExpirationPlaceholder = "[CERTIFICATE_EXPIRATION]"
 
+	// DomainExpirationPlaceholder is a placeholder for the duration before the domain expires, in milliseconds.
+	DomainExpirationPlaceholder = "[DOMAIN_EXPIRATION]"
+
 	// LengthFunctionPrefix is the prefix for the length function
 	//
 	// Usage: len([BODY].articles) == 10, len([BODY].name) > 5
@@ -142,9 +145,21 @@ func (c Condition) hasBodyPlaceholder() bool {
 	return strings.Contains(string(c), BodyPlaceholder)
 }
 
+// hasDomainExpirationPlaceholder checks whether the condition has a DomainExpirationPlaceholder
+// Used for determining whether a whois operation is necessary
+func (c Condition) hasDomainExpirationPlaceholder() bool {
+	return strings.Contains(string(c), DomainExpirationPlaceholder)
+}
+
+// hasIPPlaceholder checks whether the condition has an IPPlaceholder
+// Used for determining whether an IP lookup is necessary
+func (c Condition) hasIPPlaceholder() bool {
+	return strings.Contains(string(c), IPPlaceholder)
+}
+
 // isEqual compares two strings.
 //
-// Supports the pattern and the any functions.
+// Supports the "pat" and the "any" functions.
 // i.e. if one of the parameters starts with PatternFunctionPrefix and ends with FunctionSuffix, it will be treated like
 // a pattern.
 func isEqual(first, second string) bool {
@@ -219,6 +234,8 @@ func sanitizeAndResolve(elements []string, result *Result) ([]string, []string) 
 			element = strconv.FormatBool(result.Connected)
 		case CertificateExpirationPlaceholder:
 			element = strconv.FormatInt(result.CertificateExpiration.Milliseconds(), 10)
+		case DomainExpirationPlaceholder:
+			element = strconv.FormatInt(result.DomainExpiration.Milliseconds(), 10)
 		default:
 			// if contains the BodyPlaceholder, then evaluate json path
 			if strings.Contains(element, BodyPlaceholder) {

core/endpoint.go
@@ -16,6 +16,7 @@ import (
 	"github.com/TwiN/gatus/v4/client"
 	"github.com/TwiN/gatus/v4/core/ui"
 	"github.com/TwiN/gatus/v4/util"
+	"github.com/TwiN/whois"
 )
 
 type EndpointType string
@@ -138,7 +139,7 @@ func (endpoint Endpoint) Type() EndpointType {
 	}
 }
 
-// ValidateAndSetDefaults validates the endpoint's configuration and sets the default value of fields that have one
+// ValidateAndSetDefaults validates the endpoint's configuration and sets the default value of args that have one
 func (endpoint *Endpoint) ValidateAndSetDefaults() error {
 	// Set default values
 	if endpoint.ClientConfig == nil {
@@ -220,7 +221,26 @@ func (endpoint Endpoint) Key() string {
 // EvaluateHealth sends a request to the endpoint's URL and evaluates the conditions of the endpoint.
 func (endpoint *Endpoint) EvaluateHealth() *Result {
 	result := &Result{Success: true, Errors: []string{}}
-	endpoint.getIP(result)
+	// Parse or extract hostname from URL
+	if endpoint.DNS != nil {
+		result.Hostname = strings.TrimSuffix(endpoint.URL, ":53")
+	} else {
+		urlObject, err := url.Parse(endpoint.URL)
+		if err != nil {
+			result.AddError(err.Error())
+		} else {
+			result.Hostname = urlObject.Hostname()
+		}
+	}
+	// Retrieve IP if necessary
+	if endpoint.needsToRetrieveIP() {
+		endpoint.getIP(result)
+	}
+	// Retrieve domain expiration if necessary
+	if endpoint.needsToRetrieveDomainExpiration() && len(result.Hostname) > 0 {
+		endpoint.getDomainExpiration(result)
+	}
+	//
 	if len(result.Errors) == 0 {
 		endpoint.call(result)
 	} else {
@@ -251,22 +271,21 @@ func (endpoint *Endpoint) EvaluateHealth() *Result {
 }
 
 func (endpoint *Endpoint) getIP(result *Result) {
-	if endpoint.DNS != nil {
-		result.Hostname = strings.TrimSuffix(endpoint.URL, ":53")
-	} else {
-		urlObject, err := url.Parse(endpoint.URL)
-		if err != nil {
-			result.AddError(err.Error())
-			return
-		}
-		result.Hostname = urlObject.Hostname()
-	}
 	ips, err := net.LookupIP(result.Hostname)
 	if err != nil {
 		result.AddError(err.Error())
 		return
 	}
 	result.IP = ips[0].String()
+}
+
+func (endpoint *Endpoint) getDomainExpiration(result *Result) {
+	whoisClient := whois.NewClient()
+	if whoisResponse, err := whoisClient.QueryAndParse(result.Hostname); err != nil {
+		result.AddError("error querying and parsing hostname using whois client: " + err.Error())
+	} else {
+		result.DomainExpiration = time.Until(whoisResponse.ExpirationDate)
+	}
 }
 
 func (endpoint *Endpoint) call(result *Result) {
@@ -317,7 +336,7 @@ func (endpoint *Endpoint) call(result *Result) {
 		if endpoint.needsToReadBody() {
 			result.body, err = io.ReadAll(response.Body)
 			if err != nil {
-				result.AddError(err.Error())
+				result.AddError("error reading response body:" + err.Error())
 			}
 		}
 	}
@@ -344,10 +363,30 @@ func (endpoint *Endpoint) buildHTTPRequest() *http.Request {
 	return request
 }
 
-// needsToReadBody checks if there's any conditions that requires the response body to be read
+// needsToReadBody checks if there's any condition that requires the response body to be read
 func (endpoint *Endpoint) needsToReadBody() bool {
 	for _, condition := range endpoint.Conditions {
 		if condition.hasBodyPlaceholder() {
+			return true
+		}
+	}
+	return false
+}
+
+// needsToRetrieveDomainExpiration checks if there's any condition that requires a whois query to be performed
+func (endpoint *Endpoint) needsToRetrieveDomainExpiration() bool {
+	for _, condition := range endpoint.Conditions {
+		if condition.hasDomainExpirationPlaceholder() {
+			return true
+		}
+	}
+	return false
+}
+
+// needsToRetrieveIP checks if there's any condition that requires an IP lookup
+func (endpoint *Endpoint) needsToRetrieveIP() bool {
+	for _, condition := range endpoint.Conditions {
+		if condition.hasIPPlaceholder() {
 			return true
 		}
 	}

commit d9f86f115577cf9a00ec57801337d0cd81535893

fix(storage): Default domain_expiration to 0 for SQL when the column doesn't already exist
This will prevent temporary issues with the parsing of old results that would otherwise
have a value of NULL for domain_expiration

Fixes an issue introduced by #325

storage/store/sql/specific_postgres.go
@@ -5,8 +5,8 @@ func (s *Store) createPostgresSchema() error {
 		CREATE TABLE IF NOT EXISTS endpoints (
 			endpoint_id    BIGSERIAL PRIMARY KEY,
 			endpoint_key   TEXT UNIQUE,
-			endpoint_name  TEXT,
-			endpoint_group TEXT,
+			endpoint_name  TEXT NOT NULL,
+			endpoint_group TEXT NOT NULL,
 			UNIQUE(endpoint_name, endpoint_group)
 		)
 	`)
@@ -16,9 +16,9 @@ func (s *Store) createPostgresSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_events (
 			endpoint_event_id  BIGSERIAL PRIMARY KEY,
-			endpoint_id        INTEGER REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			event_type         TEXT,
-			event_timestamp    TIMESTAMP
+			endpoint_id        INTEGER   NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			event_type         TEXT      NOT NULL,
+			event_timestamp    TIMESTAMP NOT NULL
 		)
 	`)
 	if err != nil {
@@ -27,18 +27,18 @@ func (s *Store) createPostgresSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_results (
 			endpoint_result_id     BIGSERIAL PRIMARY KEY,
-			endpoint_id            BIGINT REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			success                BOOLEAN,
-			errors                 TEXT,
-			connected              BOOLEAN,
-			status                 BIGINT,
-			dns_rcode              TEXT,
-			certificate_expiration BIGINT,
-			domain_expiration      BIGINT,
-			hostname               TEXT,
-			ip                     TEXT,
-			duration               BIGINT,
-			timestamp              TIMESTAMP
+			endpoint_id            BIGINT    NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			success                BOOLEAN   NOT NULL,
+			errors                 TEXT      NOT NULL,
+			connected              BOOLEAN   NOT NULL,
+			status                 BIGINT    NOT NULL,
+			dns_rcode              TEXT      NOT NULL,
+			certificate_expiration BIGINT    NOT NULL,
+			domain_expiration      BIGINT    NOT NULL,
+			hostname               TEXT      NOT NULL,
+			ip                     TEXT      NOT NULL,
+			duration               BIGINT    NOT NULL,
+			timestamp              TIMESTAMP NOT NULL
 		)
 	`)
 	if err != nil {
@@ -47,9 +47,9 @@ func (s *Store) createPostgresSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_result_conditions (
 			endpoint_result_condition_id  BIGSERIAL PRIMARY KEY,
-			endpoint_result_id            BIGINT REFERENCES endpoint_results(endpoint_result_id) ON DELETE CASCADE,
-			condition                     TEXT,
-			success                       BOOLEAN
+			endpoint_result_id            BIGINT  NOT NULL REFERENCES endpoint_results(endpoint_result_id) ON DELETE CASCADE,
+			condition                     TEXT    NOT NULL,
+			success                       BOOLEAN NOT NULL
 		)
 	`)
 	if err != nil {
@@ -58,15 +58,15 @@ func (s *Store) createPostgresSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_uptimes (
 			endpoint_uptime_id     BIGSERIAL PRIMARY KEY,
-			endpoint_id            BIGINT REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			hour_unix_timestamp    BIGINT,
-			total_executions       BIGINT,
-			successful_executions  BIGINT,
-			total_response_time    BIGINT,
+			endpoint_id            BIGINT NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			hour_unix_timestamp    BIGINT NOT NULL,
+			total_executions       BIGINT NOT NULL,
+			successful_executions  BIGINT NOT NULL,
+			total_response_time    BIGINT NOT NULL,
 			UNIQUE(endpoint_id, hour_unix_timestamp)
 		)
 	`)
 	// Silent table modifications
-	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD IF NOT EXISTS domain_expiration BIGINT`)
+	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD IF NOT EXISTS domain_expiration BIGINT NOT NULL DEFAULT 0`)
 	return err
 }

storage/store/sql/specific_sqlite.go
@@ -5,8 +5,8 @@ func (s *Store) createSQLiteSchema() error {
 		CREATE TABLE IF NOT EXISTS endpoints (
 			endpoint_id    INTEGER PRIMARY KEY,
 			endpoint_key   TEXT UNIQUE,
-			endpoint_name  TEXT,
-			endpoint_group TEXT,
+			endpoint_name  TEXT NOT NULL,
+			endpoint_group TEXT NOT NULL,
 			UNIQUE(endpoint_name, endpoint_group)
 		)
 	`)
@@ -16,9 +16,9 @@ func (s *Store) createSQLiteSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_events (
 			endpoint_event_id  INTEGER PRIMARY KEY,
-			endpoint_id        INTEGER REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			event_type         TEXT,
-			event_timestamp    TIMESTAMP
+			endpoint_id        INTEGER   NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			event_type         TEXT      NOT NULL,
+			event_timestamp    TIMESTAMP NOT NULL
 		)
 	`)
 	if err != nil {
@@ -27,18 +27,18 @@ func (s *Store) createSQLiteSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_results (
 			endpoint_result_id     INTEGER PRIMARY KEY,
-			endpoint_id            INTEGER REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			success                INTEGER,
-			errors                 TEXT,
-			connected              INTEGER,
-			status                 INTEGER,
-			dns_rcode              TEXT,
-			certificate_expiration INTEGER,
-		    domain_expiration      INTEGER,
-			hostname               TEXT,
-			ip                     TEXT,
-			duration               INTEGER,
-			timestamp              TIMESTAMP
+			endpoint_id            INTEGER   NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			success                INTEGER   NOT NULL,
+			errors                 TEXT      NOT NULL,
+			connected              INTEGER   NOT NULL,
+			status                 INTEGER   NOT NULL,
+			dns_rcode              TEXT      NOT NULL,
+			certificate_expiration INTEGER   NOT NULL,
+		    domain_expiration      INTEGER   NOT NULL,
+			hostname               TEXT      NOT NULL,
+			ip                     TEXT      NOT NULL,
+			duration               INTEGER   NOT NULL,
+			timestamp              TIMESTAMP NOT NULL
 		)
 	`)
 	if err != nil {
@@ -47,9 +47,9 @@ func (s *Store) createSQLiteSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_result_conditions (
 			endpoint_result_condition_id  INTEGER PRIMARY KEY,
-			endpoint_result_id            INTEGER REFERENCES endpoint_results(endpoint_result_id) ON DELETE CASCADE,
-			condition                     TEXT,
-			success                       INTEGER
+			endpoint_result_id            INTEGER NOT NULL REFERENCES endpoint_results(endpoint_result_id) ON DELETE CASCADE,
+			condition                     TEXT    NOT NULL,
+			success                       INTEGER NOT NULL
 		)
 	`)
 	if err != nil {
@@ -58,15 +58,15 @@ func (s *Store) createSQLiteSchema() error {
 	_, err = s.db.Exec(`
 		CREATE TABLE IF NOT EXISTS endpoint_uptimes (
 			endpoint_uptime_id    INTEGER PRIMARY KEY,
-			endpoint_id           INTEGER REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
-			hour_unix_timestamp   INTEGER,
-			total_executions      INTEGER,
-			successful_executions INTEGER,
-			total_response_time   INTEGER,
+			endpoint_id           INTEGER NOT NULL REFERENCES endpoints(endpoint_id) ON DELETE CASCADE,
+			hour_unix_timestamp   INTEGER NOT NULL,
+			total_executions      INTEGER NOT NULL,
+			successful_executions INTEGER NOT NULL,
+			total_response_time   INTEGER NOT NULL,
 			UNIQUE(endpoint_id, hour_unix_timestamp)
 		)
 	`)
-	// Silent table modifications
-	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD domain_expiration INTEGER`)
+	// Silent table modifications TODO: Remove this
+	_, _ = s.db.Exec(`ALTER TABLE endpoint_results ADD domain_expiration INTEGER NOT NULL DEFAULT 0`)
 	return err
 }

storage/store/sql/sql.go
@@ -609,7 +609,11 @@ func (s *Store) getEndpointResultsByEndpointID(tx *sql.Tx, endpointID int64, pag
 		result := &core.Result{}
 		var id int64
 		var joinedErrors string
-		_ = rows.Scan(&id, &result.Success, &joinedErrors, &result.Connected, &result.HTTPStatus, &result.DNSRCode, &result.CertificateExpiration, &result.DomainExpiration, &result.Hostname, &result.IP, &result.Duration, &result.Timestamp)
+		err = rows.Scan(&id, &result.Success, &joinedErrors, &result.Connected, &result.HTTPStatus, &result.DNSRCode, &result.CertificateExpiration, &result.DomainExpiration, &result.Hostname, &result.IP, &result.Duration, &result.Timestamp)
+		if err != nil {
+			log.Printf("[sql][getEndpointResultsByEndpointID] Silently failed to retrieve endpoint result for endpointID=%d: %s", endpointID, err.Error())
+			err = nil
+		}
 		if len(joinedErrors) != 0 {
 			result.Errors = strings.Split(joinedErrors, arraySeparator)
 		}