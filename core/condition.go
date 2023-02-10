3ca8636e6eb9d1cdec772f5cb92c028a582730ab
Heitor Pascoal de Bittencourt <heitorpbittencourt@gmail.com>
2023-02-10 11:06:37 -0300

    Fix comparison of block heights
    Use an absolute comparison between our RPC node's block height and the
    reference value.

--- core/condition.go
+++ core/condition.go
@@ -399,7 +399,15 @@ func blockNum(result *Result, first, second string) bool {
 	}
 
 	result.AddError(fmt.Sprintf("rpc: %d, reference: %d", firstI, secondI))
-	return !(secondI-firstI > deltaThreshold)
+	return !(absInt(secondI-firstI) > deltaThreshold)
+}
+
+func absInt(number int64) int64 {
+	if number < 0 {
+		return -number
+	} else {
+		return number
+	}
 }
 
 // isEqual compares two strings.
