220be7111f836205c24c949a4c56a43cc336234b
Gerrit <gerrit.abma@logius.nl>
2024-07-22 10:58:24 +0200

    Expose channel configuration parameter through alerting.mattermost.channel

--- alerting/provider/mattermost/mattermost.go
+++ alerting/provider/mattermost/mattermost.go
@@ -16,6 +16,9 @@ import (
 type AlertProvider struct {
 	WebhookURL string `yaml:"webhook-url"`
 
+	// Channel is the optional setting to override the default webhook's channel
+	Channel string `yaml:"channel,omitempty"`
+
 	// ClientConfig is the configuration of the client used to communicate with the provider's target
 	ClientConfig *client.Config `yaml:"client,omitempty"`
 
@@ -119,7 +122,7 @@ func (provider *AlertProvider) buildRequestBody(ep *endpoint.Endpoint, alert *al
 		description = ":\n> " + alertDescription
 	}
 	body := Body{
-		Channel:  "",
+		Channel:  provider.Channel,
 		Text:     "",
 		Username: "gatus",
 		IconURL:  "https://raw.githubusercontent.com/TwiN/gatus/master/.github/assets/logo.png",
