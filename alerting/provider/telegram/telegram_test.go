33ffc11e72aae65a57839f2bb43b4f4830ea5f4d
TwiN <twin@linux.com>
2024-07-27 19:00:01 -0400

    Update alerting/provider/telegram/telegram_test.go

--- alerting/provider/telegram/telegram_test.go
+++ alerting/provider/telegram/telegram_test.go
@@ -82,7 +82,7 @@ func TestAlertProvider_getTokenAndIDForGroup(t *testing.T) {
 			t.Error("id should have been 'overrideID'")
 		}
 	})
-	t.Run("get-default-token-with-override", func(t *testing.T) {
+	t.Run("get-default-token-with-overridden-token", func(t *testing.T) {
 		provider := AlertProvider{Token: "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11", ID: "12345678", Overrides: []*Override{{group: "group", token: "overrideToken"}}}
 		token := provider.getTokenForGroup("group")
 		if token != "overrideToken" {
