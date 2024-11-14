dc2e1ecdbb0f93554e69571cd136bf3b1d5305bd
TwiN <twin@linux.com>
2024-11-13 23:43:55 -0500

    Improve log message if GATUS_LOG_LEVEL isn't set

--- main.go
+++ main.go
@@ -68,7 +68,11 @@ func configureLogging() {
 	logLevelAsString := os.Getenv(GatusLogLevelEnvVar)
 	if logLevel, err := logr.LevelFromString(logLevelAsString); err != nil {
 		logr.SetThreshold(logr.LevelInfo)
-		logr.Warnf("[main.configureLogging] Invalid log level '%s', defaulting to %s", logLevelAsString, logr.LevelInfo)
+		if len(logLevelAsString) == 0 {
+			logr.Infof("[main.configureLogging] Defaulting log level to %s", logr.LevelInfo)
+		} else {
+			logr.Warnf("[main.configureLogging] Invalid log level '%s', defaulting to %s", logLevelAsString, logr.LevelInfo)
+		}
 	} else {
 		logr.SetThreshold(logLevel)
 		logr.Infof("[main.configureLogging] Log Level is set to %s", logr.GetThreshold())
