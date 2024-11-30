41428089a8a6e9d72a87c1b6f903b146235c6236
Bishtawi <bishtawi@users.noreply.github.com>
2024-11-29 22:48:31 -0800

    Add overrides to Ntfy alert provider

--- alerting/provider/ntfy/ntfy.go
+++ alerting/provider/ntfy/ntfy.go
@@ -17,6 +17,7 @@ import (
 const (
 	DefaultURL      = "https://ntfy.sh"
 	DefaultPriority = 3
+	TokenPrefix     = "tk_"
 )
 
 // AlertProvider is the configuration necessary for sending an alert using Slack
@@ -32,6 +33,20 @@ type AlertProvider struct {
 
 	// DefaultAlert is the default alert configuration to use for endpoints with an alert of the appropriate type
 	DefaultAlert *alert.Alert `yaml:"default-alert,omitempty"`
+
+	// Overrides is a list of Override that may be prioritized over the default configuration
+	Overrides []Override `yaml:"overrides,omitempty"`
+}
+
+// Override is a case under which the default integration is overridden
+type Override struct {
+	Group    string `yaml:"group"`
+	Topic    string `yaml:"topic"`
+	URL      string `yaml:"url"`
+	Priority int    `yaml:"priority"`
+	Token    string `yaml:"token"`
+	Email    string `yaml:"email"`
+	Click    string `yaml:"click"`
 }
 
 // IsValid returns whether the provider's configuration is valid
@@ -44,21 +59,43 @@ func (provider *AlertProvider) IsValid() bool {
 	}
 	isTokenValid := true
 	if len(provider.Token) > 0 {
-		isTokenValid = strings.HasPrefix(provider.Token, "tk_")
+		isTokenValid = strings.HasPrefix(provider.Token, TokenPrefix)
 	}
+
+	registeredGroups := make(map[string]bool)
+	if provider.Overrides != nil {
+		for _, override := range provider.Overrides {
+			if len(override.Group) == 0 {
+				return false
+			}
+			if _, ok := registeredGroups[override.Group]; ok {
+				return false
+			}
+			if len(override.Token) > 0 && !strings.HasPrefix(override.Token, TokenPrefix) {
+				return false
+			}
+			if override.Priority < 0 || override.Priority >= 6 {
+				return false
+			}
+			registeredGroups[override.Group] = true
+		}
+	}
+
 	return len(provider.URL) > 0 && len(provider.Topic) > 0 && provider.Priority > 0 && provider.Priority < 6 && isTokenValid
 }
 
 // Send an alert using the provider
 func (provider *AlertProvider) Send(ep *endpoint.Endpoint, alert *alert.Alert, result *endpoint.Result, resolved bool) error {
-	buffer := bytes.NewBuffer(provider.buildRequestBody(ep, alert, result, resolved))
-	request, err := http.NewRequest(http.MethodPost, provider.URL, buffer)
+	override := provider.getGroupOverride(ep.Group)
+	buffer := bytes.NewBuffer(provider.buildRequestBody(ep, alert, result, resolved, override))
+	url := provider.getURL(override)
+	request, err := http.NewRequest(http.MethodPost, url, buffer)
 	if err != nil {
 		return err
 	}
 	request.Header.Set("Content-Type", "application/json")
-	if len(provider.Token) > 0 {
-		request.Header.Set("Authorization", "Bearer "+provider.Token)
+	if token := provider.getToken(override); len(token) > 0 {
+		request.Header.Set("Authorization", "Bearer "+token)
 	}
 	if provider.DisableFirebase {
 		request.Header.Set("Firebase", "no")
@@ -89,7 +126,7 @@ type Body struct {
 }
 
 // buildRequestBody builds the request body for the provider
-func (provider *AlertProvider) buildRequestBody(ep *endpoint.Endpoint, alert *alert.Alert, result *endpoint.Result, resolved bool) []byte {
+func (provider *AlertProvider) buildRequestBody(ep *endpoint.Endpoint, alert *alert.Alert, result *endpoint.Result, resolved bool, override *Override) []byte {
 	var message, formattedConditionResults, tag string
 	if resolved {
 		tag = "white_check_mark"
@@ -112,13 +149,13 @@ func (provider *AlertProvider) buildRequestBody(ep *endpoint.Endpoint, alert *al
 	}
 	message += formattedConditionResults
 	body, _ := json.Marshal(Body{
-		Topic:    provider.Topic,
+		Topic:    provider.getTopic(override),
 		Title:    "Gatus: " + ep.DisplayName(),
 		Message:  message,
 		Tags:     []string{tag},
-		Priority: provider.Priority,
-		Email:    provider.Email,
-		Click:    provider.Click,
+		Priority: provider.getPriority(override),
+		Email:    provider.getEmail(override),
+		Click:    provider.getClick(override),
 	})
 	return body
 }
@@ -126,4 +163,57 @@ func (provider *AlertProvider) buildRequestBody(ep *endpoint.Endpoint, alert *al
 // GetDefaultAlert returns the provider's default alert configuration
 func (provider *AlertProvider) GetDefaultAlert() *alert.Alert {
 	return provider.DefaultAlert
+}
+
+func (provider *AlertProvider) getGroupOverride(group string) *Override {
+	if provider.Overrides != nil {
+		for _, override := range provider.Overrides {
+			if group == override.Group {
+				return &override
+			}
+		}
+	}
+	return nil
+}
+
+func (provider *AlertProvider) getTopic(override *Override) string {
+	if override != nil && len(override.Topic) > 0 {
+		return override.Topic
+	}
+	return provider.Topic
+}
+
+func (provider *AlertProvider) getURL(override *Override) string {
+	if override != nil && len(override.URL) > 0 {
+		return override.URL
+	}
+	return provider.URL
+}
+
+func (provider *AlertProvider) getPriority(override *Override) int {
+	if override != nil && override.Priority > 0 {
+		return override.Priority
+	}
+	return provider.Priority
+}
+
+func (provider *AlertProvider) getToken(override *Override) string {
+	if override != nil && len(override.Token) > 0 {
+		return override.Token
+	}
+	return provider.Token
+}
+
+func (provider *AlertProvider) getEmail(override *Override) string {
+	if override != nil && len(override.Email) > 0 {
+		return override.Email
+	}
+	return provider.Email
+}
+
+func (provider *AlertProvider) getClick(override *Override) string {
+	if override != nil && len(override.Click) > 0 {
+		return override.Click
+	}
+	return provider.Click
 }
