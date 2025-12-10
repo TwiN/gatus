a55eb7da406fe7d4ec3bd19845eace0d7dde4132
PythonGermany <97847597+PythonGermany@users.noreply.github.com>
2025-12-10 22:29:46 +0100

    refactor(ui): Improve dev environment (#1429)

--- web/app/src/views/SuiteDetails.vue
+++ web/app/src/views/SuiteDetails.vue
@@ -154,7 +154,6 @@ import StepDetailsModal from '@/components/StepDetailsModal.vue'
 import Settings from '@/components/Settings.vue'
 import Loading from '@/components/Loading.vue'
 import { generatePrettyTimeAgo } from '@/utils/time'
-import { SERVER_URL } from '@/main'
 
 const router = useRouter()
 const route = useRoute()
@@ -191,7 +190,7 @@ const fetchData = async () => {
   }
 
   try {
-    const response = await fetch(`${SERVER_URL}/api/v1/suites/${route.params.key}/statuses`, {
+    const response = await fetch(`/api/v1/suites/${route.params.key}/statuses`, {
       credentials: 'include'
     })
 
