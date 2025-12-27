48f4339424e756f5fe00327e9ff2afb07520c689
qianlongzt <18493471+qianlongzt@users.noreply.github.com>
2025-12-27 22:04:27 +0800

    test(endpoint): move `BenchmarkEndpoint_Type` to file `endpoint_bench_test.go`

--- config/endpoint/endpoint_test.go
+++ config/endpoint/endpoint_test.go
@@ -431,20 +431,6 @@ func TestEndpoint_Type(t *testing.T) {
 	}
 }
 
-func BenchmarkEndpoint_Type(b *testing.B) {
-	for b.Loop() {
-		for _, tt := range testEndpoint_typeData {
-			endpoint := Endpoint{
-				URL:       tt.args.URL,
-				DNSConfig: tt.args.DNS,
-			}
-			if got := endpoint.Type(); got != tt.want {
-				b.Errorf("Endpoint.Type() = %v, want %v", got, tt.want)
-			}
-		}
-	}
-}
-
 func TestEndpoint_ValidateAndSetDefaults(t *testing.T) {
 	endpoint := Endpoint{
 		Name:               "website-health",
