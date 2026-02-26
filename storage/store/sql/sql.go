85043ca6053ae887444003abe7689acf8bd640dc
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2026-02-26 12:18:28 +0100

    support 365d badges

--- storage/store/sql/sql.go
+++ storage/store/sql/sql.go
@@ -34,8 +34,8 @@ const (
 	resultsAboveMaximumCleanUpThreshold = 10 // Maximum number of results above the configured maximum before triggering a cleanup
 
 	uptimeTotalEntriesMergeThreshold = 100                 // Maximum number of uptime entries before triggering a merge
-	uptimeAgeCleanUpThreshold        = 32 * 24 * time.Hour // Maximum uptime age before triggering a cleanup
-	uptimeRetention                  = 30 * 24 * time.Hour // Minimum duration that must be kept to operate as intended
+	uptimeAgeCleanUpThreshold        = 367 * 24 * time.Hour // Maximum uptime age before triggering a cleanup
+	uptimeRetention                  = 365 * 24 * time.Hour // Minimum duration that must be kept to operate as intended
 	uptimeHourlyBuffer               = 48 * time.Hour      // Number of hours to buffer from now when determining which hourly uptime entries can be merged into daily uptime entries
 
 	cacheTTL = 10 * time.Minute
